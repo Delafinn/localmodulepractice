41a08e712fd4fc3972c41b822d3f78546c54df9a
Finn <32446445+Delafinn@users.noreply.github.com>
2021-11-28 16:51:01 -0700

    Add files via upload
    import practice takes name lists and just prints out the names. this is the name list from https://github.com/Delafinn/CharacterGenerator/blob/main/characterGenerator.py

---
+++ import practice.py
@@ -0,0 +1,8 @@
+# learning to import local modules
+from namelist import *
+import time
+while True:
+    gq = input("do you want to display the list of names?")
+    if gq == "yes" or gq ==" yes" or gq == "y" or gq == "Y":
+        print(namelist)
+    time.sleep(3)
---
+++ namelist.py
@@ -0,0 +1,2 @@
+namelist = ["alvin", "bob", "robert", "obiwan", "bill", "Morpheus", "lisa", "annie", "aang", "katara", "momo", "fred", "lola", "patrick", "pascal","Yoda","bill", "Darth","Rocco", "Rafael","Francis","Hugo"
+ ,"Justin", "Solomon", "Ryan", "Milo", "Layla", "Carmen", "Olivia", "Evelyn", "Edie", "Tia"]
